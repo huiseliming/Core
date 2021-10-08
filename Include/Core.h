b611e9570cc7cb11694d3ad26561234fdba0fa76
huiseliming <1055907692@qq.com>
2021-10-08 11:08:42 +0800

    rm format from header

--- Include/Core.h
+++ Include/Core.h
@@ -6,19 +6,8 @@
 #ifdef  _WIN32
 #define WIN32_LEAN_AND_MEAN 
 #include <Windows.h>
-#ifdef __REFLECTOR__ // reflector using clang, c++23 std::format not impl
-#ifndef STD_FORMAT_EMPTY_DECL
-namespace std {
-	template <class... _Types>
-	string format(const string_view _Fmt, const _Types&... _Args) {
-		return {};
-	}
-}
-#define STD_FORMAT_EMPTY_DECL
-#endif
-#else
-#include <format>
-#endif
+
+
 
 
 
